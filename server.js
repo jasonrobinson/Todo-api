a806136b7e8d149112129fa1be163dbd8235ac76
Jason Robinson <jcr.compstat@gmail.com>
2016-08-25 16:56:33 -0700

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/todos', function (req, res) {
     // console.log('description: ' + body.description);
 
     res.json(body);
-})
+});
 
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
@@ -60,7 +60,34 @@ app.delete('/todos/:id', function (req, res) {
         // res.status(200).send();
     }
 
-})
+});
+
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);  // passed by reference, so modified
+    res.json(matchedTodo);
+
+});
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
