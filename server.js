7772c4bb452fafe01840c6fbafacc6ebb53e346c
Jason Robinson <jcr.compstat@gmail.com>
2016-08-26 10:48:31 -0700

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -110,15 +110,39 @@ app.post('/todos', function (req, res) {
 
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+    // db.todo.findById(todoId).then(function(todo) {
+    //     if (!!todo) {
+    //         res.json(todo);
+    //         todo.destroy();
+    //     } else {
+    //         res.status(404).send();
+    //     }
+    // }, function (e) {
+    //     res.status(500).send();
+    // });
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function (rowsDeleted) {
+        if (rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with id'
+            });
+        } else {
+            res.status(204).send();  // OK and no response to send
+        }
+    });
 
-    if (!matchedTodo) {
-        res.status(404).json({"error": "no todo found with that id"});
-    } else {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-        // res.status(200).send();
-    }
+    // var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    // if (!matchedTodo) {
+    //     res.status(404).json({"error": "no todo found with that id"});
+    // } else {
+    //     todos = _.without(todos, matchedTodo);
+    //     res.json(matchedTodo);
+    //     // res.status(200).send();
+    // }
 
 });
 
