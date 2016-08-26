a6fc8d1aa743b4e0b8ee9fb22c878098959d6bbf
Jason Robinson <jcr.compstat@gmail.com>
2016-08-26 11:21:54 -0700

    Update PUT /todos/:id to postgres

--- server.js
+++ server.js
@@ -148,28 +148,54 @@ app.delete('/todos/:id', function (req, res) {
 
 app.put('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+    // var matchedTodo = _.findWhere(todos, {id: todoId});
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
+    // var validAttributes = {};
 
-    if (!matchedTodo) {
-        return res.status(404).send();
-    }
+    // if (!matchedTodo) {
+    //     return res.status(404).send();
+    // }
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).send();
-    }
+    if (body.hasOwnProperty('completed')) { // && _.isBoolean(body.completed)) {
+        attributes.completed = body.completed;
+        // validAttributes.completed = body.completed;
+    } // else if (body.hasOwnProperty('completed')) {
+    //     return res.status(400).send();
+    // }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
-    }
+    if (body.hasOwnProperty('description')) { // && _.isString(body.description) && body.description.trim().length > 0) {
+        attributes.description = body.description;
+        // validAttributes.description = body.description;
+    } // else if (body.hasOwnProperty('description')) {
+    //     return res.status(400).send();
+    // }
 
-    _.extend(matchedTodo, validAttributes);  // passed by reference, so modified
-    res.json(matchedTodo);
+    // _.extend(matchedTodo, attributes);  // validAttributes);  // passed by reference, so modified
+    // res.json(matchedTodo);
+
+    db.todo.findById(todoId).then(function (todo) {
+        // if (todo) {
+        //     return todo.update(attributes);
+        // } else {
+        //     res.status(404).send();
+        // }
+        if (todo) {
+            return todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON());
+            }, function (e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function () {
+        res.status(500).send();
+    // }).then(function (todo) {
+    //     res.json(todo.toJSON());
+    // }, function (e) {
+    //     res.status(400).json(e);
+    });
 
 });
 
