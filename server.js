44836c3e8e18f444f1601f7acad644d759a00709
Jason Robinson <jcr.compstat@gmail.com>
2016-08-26 21:30:32 -0700

    Convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function (req, res) {
 // GET /todos
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
     var query = req.query;
-    var where = {};
+    var where = {
+        userId: req.user.get('id')
+    };
 
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
         where.completed = true;
@@ -62,7 +64,12 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    db.todo.findById(todoId).then(function (todo) {
+    // db.todo.findById(todoId).then(function (todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }}).then(function (todo) {
         if (!!todo) {
             res.json(todo.toJSON());
         } else {
@@ -127,7 +134,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
     // });
     db.todo.destroy({
         where: {
-            id: todoId
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function (rowsDeleted) {
         if (rowsDeleted === 0) {
@@ -179,7 +187,13 @@ app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
     // _.extend(matchedTodo, attributes);  // validAttributes);  // passed by reference, so modified
     // res.json(matchedTodo);
 
-    db.todo.findById(todoId).then(function (todo) {
+    // db.todo.findById(todoId).then(function (todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function (todo) {
         // if (todo) {
         //     return todo.update(attributes);
         // } else {
